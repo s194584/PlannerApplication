6bb7eb3068bd2478024d53c50194ca9dcb8d7e76
s194584 <59605730+s194584@users.noreply.github.com>
2020-04-13 11:42:26 +0200

    Changed README.md
    Added comments

--- src/planner/Main.java
+++ src/planner/Main.java
@@ -6,10 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 public class Main extends Application {
     // To run javaFX; --module-path ${PATH_TO_FX11} --add-modules=javafx.controls,javafx.fxml
     @Override
     public void start(Stage primaryStage) throws Exception{
+        // The resource folder has to be marked as such in project structure
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
